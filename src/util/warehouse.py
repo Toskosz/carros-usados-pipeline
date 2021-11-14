1afc00d462c58df7dadee4dc7bf4f463005fb9ec
toskosz <thiago.tokarski2001@gmail.com>
2021-11-14 14:56:23 -0300

    DOC comments on tricky parts

--- src/util/warehouse.py
+++ src/util/warehouse.py
@@ -1,10 +1,10 @@
 from contextlib import contextmanager
 from dataclasses import dataclass
-from typing import final
 
 import psycopg2
-from sqlalchemy import create_engine
 
+# @dataclass used for reduced code repetition and cost
+# for more information: https://pt.stackoverflow.com/questions/376306/o-que-s%C3%A3o-dataclasses-e-quando-utiliz%C3%A1-las
 @dataclass
 class DBConnection:
     db: str
@@ -20,12 +20,18 @@ class WarehouseConnection:
             f'{db_conn.host}:{db_conn.port}/{db_conn.db}'
         )
 
+    # Python's resource manager, automatically managing resources
+    # for more information: https://www.geeksforgeeks.org/context-manager-using-contextmanager-decorator/
     @contextmanager
     def managed_cursor(self, cursor_factory=None):
         self.conn = psycopg2.connect(self.conn_url)
+
+        # autocommit mode: this way all the commands executed will be immediately committed and no rollback is possible.
         self.conn.autocommit = True
         self.curr = self.conn.cursor(cursor_factory=cursor_factory)
         try:
+            # yield is a keyword that is used like return, except the function will return a generator.
+            # for more: https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
             yield self.curr
         finally:
             self.curr.close()
