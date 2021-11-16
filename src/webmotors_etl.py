f75e955465529da0b821c56b98d1186d31546a70
toskosz <thiago.tokarski2001@gmail.com>
2021-11-16 13:03:31 -0300

    REFACTORING transformed the code to a extract only class that extract the data and store it raw in a csv file in the local file system (maybe to S3 in the future)

--- src/webmotors_etl.py
+++ src/webmotors_etl.py
@@ -82,55 +82,9 @@ from util.warehouse import WarehouseConnection
 }
 '''
 
-class WebmotorsETL:
+class WebmotorsExtract:
 
     def __init__(self) -> None:
-        self.dummy_columns = {
-            'Aceita troca':'ACEITA_TROCA',
-            'Alienado':'ALIENADO',
-            'Garantia de fábrica':'GARANTIA_DE_FABRICA',
-            'IPVA pago':'IPVA_PAGO',
-            'Licenciado':'LICENCIADO',
-            'Todas as revisões feitas pela agenda do carro':'REVISOES_PELA_AGENDA_CARRO',
-            'Todas as revisões feitas pela concessionária':'REVISOES_PELA_CONCESSIONARIA',
-            'Único dono':'UNICO_DONO',
-            'Airbag':'AIRBAG',
-            'Alarme':'ALARME',
-            'Ar condicionado':'AR_CONDICIONADO',
-            'Ar quente':'AR_QUENTE',
-            'Banco com regulagem de altura':'BANCO_REGULA_ALTURA',
-            'Bancos dianteiros com aquecimento':'BANCO_COM_AQUECIMENTO',
-            'Bancos em couro':'BANCO_DE_COURO',
-            'Capota marítima':'CAPOTA_MARITIMA',
-            'CD e mp3 player':'MP3_CD_PLAYER',
-            'CD player':'CD_PLAYER',
-            'Computador de bordo':'COMPUTAR_DE_BORDO',
-            'Controle automático de velocidade':'CONTROLE_AUTOMATICO_VEL',
-            'Controle de tração':'CONTROLE_TRACAO',
-            'Desembaçador traseiro':'DESEMBACADOR_TRASEIRO',
-            'Direção hidráulica':'DIR_HIDRAULICA',
-            'Disqueteira':'DISQUETEIRA',
-            'DVD player':'DVD_PLAYER',
-            'Encosto de cabeça traseiro':'ENCOSTO_CABECA_TRASEIRO',
-            'Farol de xenônio':'FAROL_DE_XENONIO',
-            'Freio abs':'FREIO_ABS',
-            'GPS':'GPS',
-            'Limpador traseiro':'LIMPADOR_TRASEIRO',
-            'Protetor de caçamba':'PROTETOR_CACAMBA',
-            'Rádio':'RADIO',
-            'Rádio e toca fitas':'RADIO_TOCAFICA',
-            'Retrovisor fotocrômico':'RETROVISOR_FOTOCROMICO',
-            'Retrovisores elétricos':'RETROVISOR_ELETRICO',
-            'Rodas de liga leve':'RODAS_LIGA_LEVE',
-            'Sensor de chuva':'SENSOR_DE_CHUVA',
-            'Sensor de estacionamento':'SENSOR_DE_ESTACIONAMENTO',
-            'Teto solar':'TETO_SOLAR',
-            'Tração 4x4':'TRACAO_QUATRO_POR_QUATRO',
-            'Travas elétricas':'TRAVAS_ELETRICAS',
-            'Vidros elétricos':'VIDROS_ELETRICOS',
-            'Volante com regulagem de altura':'VOLANTE_REG_ALTURA'
-        }
-
         self.req_headers = {
             'Accept':	'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Encoding': 'gzip, deflate, br',
@@ -145,38 +99,10 @@ class WebmotorsETL:
             'User-Agent':	'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0'
         }
 
-        self.columns_func_assigns = {
-            "TITULO": [self.clean_str_column],
-            "FABRICANTE": [self.clean_str_column],
-            "MODELO": [self.clean_str_column],
-            "VERSAO": [self.clean_str_column],
-            "ANO_FABRICACAO": [self.to_str],
-            "ANO_MODELO": [self.to_str],
-            "KILOMETRAGEM":[self.to_float],
-            "TRANSMISSAO": [self.clean_str_column],
-            "QNTD_PORTAS": [self.to_str],
-            "CORPO_VEICULO": [self.clean_str_column],
-            "BLINDADO": [self.compute_blindado_webmotors],
-            "COR": [self.clean_str_column],
-            "TIPO_VENDEDOR": [self.clean_str_column],
-            "CIDADE_VENDEDOR": [self.clean_str_column],
-            "ESTADO_VENDEDOR": [self.clean_str_column],
-            "UF_VENDEDOR": [self.clean_str_column],
-            "TIPO_ANUNCIO": [self.clean_str_column],
-            "ENTREGA_CARRO": [self.compute_bool],
-            "TROCA_COM_TROCO": [self.compute_bool],
-            "PRECO": [self.to_float],
-            "PORCENTAGEM_FIPE": [self.to_float],
-            "COMBUSTIVEL": [self.clean_str_column],
-            "COMENTARIO_DONO": [self.clean_str_column]
-        }
-
     def run(self) -> None:
         data = self.get_recent_cars()
-        clean_data = self.clean_data(data)
-        clean_data['DATA_CARGA'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        with WarehouseConnection(get_warehouse_creds()).managed_cursor() as curr:
-            p.execute_batch(curr, self._get_exchange_insert_query(), clean_data.values)
+        # todo: data to csv(or other format) and store it into some directory(in the future in the S3)
+        
 
 #   Extraction part
 
@@ -196,11 +122,9 @@ class WebmotorsETL:
             tmp_row['CORPO_VEICULO'] = specs['BodyType']
         
         if 'VehicleAttributes' in specs.keys():
-            atributos = specs['VehicleAttributes']
-            for atributo in atributos:
-                for _, atributo_desc in atributo.items():
-                    column_name = self.dummy_columns[atributo_desc]
-                    tmp_row[column_name] = 1
+            # list of dicts
+            tmp_row['ATRIBUTOS'] = specs['VehicleAttributes']
+            
             
         tmp_row['BLINDADO'] = specs['Armored']
         tmp_row['COR'] = specs['Color']['Primary']
@@ -216,9 +140,6 @@ class WebmotorsETL:
             tmp_row['CIDADE_VENDEDOR'] = vendedor['City']
 
         tmp_row['ESTADO_VENDEDOR'] = vendedor['State']
-
-        tmp_row['UF_VENDEDOR'] = tmp_row['ESTADO_VENDEDOR'][tmp_row['ESTADO_VENDEDOR'].find("(")+1:tmp_row['ESTADO_VENDEDOR'].find(")")]
-        tmp_row['ESTADO_VENDEDOR'] = tmp_row['ESTADO_VENDEDOR'][:tmp_row['ESTADO_VENDEDOR'].find("(")]
 
         tmp_row['AD_TYPE'] = vendedor['AdType']['Value']
         tmp_row['SCORE_VENDEDOR'] = vendedor['DealerScore']
@@ -242,11 +163,7 @@ class WebmotorsETL:
 
         tmp_row['COMBUSTIVEL'] = tmp_specs['Fuel']
 
-        optionals = tmp_specs['Optionals']
-
-        for option in optionals:
-            column_name = self.dummy_columns[option['Name']]
-            tmp_row[column_name] = 1
+        tmp_row['OPTIONALS'] = tmp_specs['Optionals']
 
         return tmp_row
 
@@ -281,15 +198,11 @@ class WebmotorsETL:
     def get_recent_cars(self) -> pd.DataFrame:
         # DataFrame for batching
         carros_webmotors = pd.DataFrame(columns=['AD_ID','TITULO','FABRICANTE','MODELO','VERSAO','ANO_FABRICACAO','ANO_MODELO','KILOMETRAGEM','TRANSMISSAO','QNTD_PORTAS','CORPO_VEICULO',
-                'ACEITA_TROCA','ALIENADO','GARANTIA_DE_FABRICA','IPVA_PAGO','LICENCIADO','REVISOES_PELA_AGENDA_CARRO','REVISOES_PELA_CONCESSIONARIA','UNICO_DONO','BLINDADO','COR','TIPO_VENDEDOR',
-                'CIDADE_VENDEDOR','ESTADO_VENDEDOR','UF_VENDEDOR','AD_TYPE','SCORE_VENDEDOR','ENTREGA_CARRO','TROCA_COM_TROCO','PRECO','PRECO_DESEJADO','COMENTARIO_DONO','PORCENTAGEM_FIPE','AIRBAG',
-                'ALARME','AR_CONDICIONADO','AR_QUENTE','BANCO_REGULA_ALTURA','BANCO_COM_AQUECIMENTO','BANCO_DE_COURO','CAPOTA_MARITIMA','MP3_CD_PLAYER','CD_PLAYER','COMPUTAR_DE_BORDO',
-                'CONTROLE_AUTOMATICO_VEL','CONTROLE_TRACAO','DESEMBACADOR_TRASEIRO','DIR_HIDRAULICA','DISQUETEIRA','DVD_PLAYER','ENCOSTO_CABECA_TRASEIRO','FAROL_DE_XENONIO','FREIO_ABS',
-                'GPS','LIMPADOR_TRASEIRO','PROTETOR_CACAMBA','RADIO','RADIO_TOCAFICA','RETROVISOR_FOTOCROMICO','RETROVISOR_ELETRICO','RODAS_LIGA_LEVE','SENSOR_DE_CHUVA',
-                'SENSOR_DE_ESTACIONAMENTO','TETO_SOLAR','TRACAO_QUATRO_POR_QUATRO','TRAVAS_ELETRICAS','VIDROS_ELETRICOS','VOLANTE_REG_ALTURA','COMBUSTIVEL'])
+        'ATRIBUTOS','BLINDADO','COR','TIPO_VENDEDOR','CIDADE_VENDEDOR','ESTADO_VENDEDOR','AD_TYPE','SCORE_VENDEDOR','ENTREGA_CARRO','TROCA_COM_TROCO','PRECO','PRECO_DESEJADO','COMENTARIO_DONO',
+        'PORCENTAGEM_FIPE','COMBUSTIVEL'])
 
         # requisitions counter, for the ETL we want to make 300
-        timeout = time.time() + 60*30   # 5 minutes from now
+        timeout = time.time() + 60*30   # 30 minutes from now
         contador = 1
 
         while True:
@@ -320,193 +233,3 @@ class WebmotorsETL:
         carros_webmotors.drop_duplicates(inplace=True)
         
         return carros_webmotors
-
-#   Transforming part
-
-    def clean_data(self,data):
-        # keeping it functional programming(?)
-        resulting_data = data.copy()
-
-        for coluna, lst_f in self.columns_func_assigns.items():
-            for f in lst_f:
-                resulting_data[coluna] = f(resulting_data[coluna])
-        
-        # if dummy columns = 0 zero then car doesnt have feature so "0"
-        resulting_data[self.dummy_columns] = resulting_data[self.dummy_columns].fillna(value=0)
-        
-        # data unavailable
-        resulting_data = resulting_data.fillna(value="INDISPONIVEL")
-        del resulting_data['OBSERVACOES']
-        return resulting_data
-
-    def clean_str_column(column):
-        # removes accents
-        normalized_column = column.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-        # return uppercase
-        return normalized_column.str.upper()
-
-    def to_upper(column):
-        # return uppercase
-        return column.str.upper()
-    
-    def to_str(column):
-        return column.astype(str)
-
-    def compute_blindado_webmotors(column):
-        return np.where(column == 'N', 0, 1)
-
-    def compute_bool(column):
-        return np.where(column == 'true', 1, 0)
-
-    def to_float(column):
-        return column.astype(float)
-
-#   Loading part
-    
-    def _get_exchange_insert_query(self) -> str:
-        return '''
-        INSERT INTO STG.WEBMOTORS (
-            AD_ID,
-            TITULO,
-            FABRICANTE,
-            MODELO,
-            VERSAO,
-            ANO_FABRICACAO,
-            ANO_MODELO,
-            KILOMETRAGEM,
-            TRANSMISSAO,
-            QNTD_PORTAS,
-            CORPO_VEICULO,
-            ACEITA_TROCA,
-            ALIENADO,
-            GARANTIA_DE_FABRICA,
-            IPVA_PAGO,
-            LICENCIADO,
-            REVISOES_PELA_AGENDA_CARRO,
-            REVISOES_PELA_CONCESSIONARIA,
-            UNICO_DONO,
-            BLINDADO,
-            COR,
-            TIPO_VENDEDOR,
-            CIDADE_VENDEDOR,
-            ESTADO_VENDEDOR,
-            UF_VENDEDOR,
-            AD_TYPE,
-            SCORE_VENDEDOR,
-            ENTREGA_CARRO,
-            TROCA_COM_TROCO,
-            PRECO,
-            PRECO_DESEJADO,
-            COMENTARIO_DONO,
-            PORCENTAGEM_FIPE,
-            AIRBAG,
-            ALARME,
-            AR_CONDICIONADO,
-            AR_QUENTE,
-            BANCO_REGULA_ALTURA,
-            BANCO_COM_AQUECIMENTO,
-            BANCO_DE_COURO,
-            CAPOTA_MARITIMA,
-            MP3_CD_PLAYER,
-            CD_PLAYER,
-            COMPUTAR_DE_BORDO,
-            CONTROLE_AUTOMATICO_VEL,
-            CONTROLE_TRACAO,
-            DESEMBACADOR_TRASEIRO,
-            DIR_HIDRAULICA,
-            DISQUETEIRA,
-            DVD_PLAYER,
-            ENCOSTO_CABECA_TRASEIRO,
-            FAROL_DE_XENONIO,
-            FREIO_ABS,
-            GPS,
-            LIMPADOR_TRASEIRO,
-            PROTETOR_CACAMBA,
-            RADIO,
-            RADIO_TOCAFICA,
-            RETROVISOR_FOTOCROMICO,
-            RETROVISOR_ELETRICO,
-            RODAS_LIGA_LEVE,
-            SENSOR_DE_CHUVA,
-            SENSOR_DE_ESTACIONAMENTO,
-            TETO_SOLAR,
-            TRACAO_QUATRO_POR_QUATRO,
-            TRAVAS_ELETRICAS,
-            VIDROS_ELETRICOS,
-            VOLANTE_REG_ALTURA,
-            COMBUSTIVEL,
-            DATA_CARGA
-        )
-        VALUES (
-            %(AD_ID)s,
-            %(TITULO)s,
-            %(FABRICANTE)s,
-            %(MODELO)s,
-            %(VERSAO)s,
-            %(ANO_FABRICACAO)s,
-            %(ANO_MODELO)s,
-            %(KILOMETRAGEM)s,
-            %(TRANSMISSAO)s,
-            %(QNTD_PORTAS)s,
-            %(CORPO_VEICULO)s,
-            %(ACEITA_TROCA)s,
-            %(ALIENADO)s,
-            %(GARANTIA_DE_FABRICA)s,
-            %(IPVA_PAGO)s,
-            %(LICENCIADO)s,
-            %(REVISOES_PELA_AGENDA_CARRO)s,
-            %(REVISOES_PELA_CONCESSIONARIA)s,
-            %(UNICO_DONO)s,
-            %(BLINDADO)s,
-            %(COR)s,
-            %(TIPO_VENDEDOR)s,
-            %(CIDADE_VENDEDOR)s,
-            %(ESTADO_VENDEDOR)s,
-            %(UF_VENDEDOR)s,
-            %(AD_TYPE)s,
-            %(SCORE_VENDEDOR)s,
-            %(ENTREGA_CARRO)s,
-            %(TROCA_COM_TROCO)s,
-            %(PRECO)s,
-            %(PRECO_DESEJADO)s,
-            %(COMENTARIO_DONO)s,
-            %(PORCENTAGEM_FIPE)s,
-            %(AIRBAG)s,
-            %(ALARME)s,
-            %(AR_CONDICIONADO)s,
-            %(AR_QUENTE)s,
-            %(BANCO_REGULA_ALTURA)s,
-            %(BANCO_COM_AQUECIMENTO)s,
-            %(BANCO_DE_COURO)s,
-            %(CAPOTA_MARITIMA)s,
-            %(MP3_CD_PLAYER)s,
-            %(CD_PLAYER)s,
-            %(COMPUTAR_DE_BORDO)s,
-            %(CONTROLE_AUTOMATICO_VEL)s,
-            %(CONTROLE_TRACAO)s,
-            %(DESEMBACADOR_TRASEIRO)s,
-            %(DIR_HIDRAULICA)s,
-            %(DISQUETEIRA)s,
-            %(DVD_PLAYER)s,
-            %(ENCOSTO_CABECA_TRASEIRO)s,
-            %(FAROL_DE_XENONIO)s,
-            %(FREIO_ABS)s,
-            %(GPS)s,
-            %(LIMPADOR_TRASEIRO)s,
-            %(PROTETOR_CACAMBA)s,
-            %(RADIO)s,
-            %(RADIO_TOCAFICA)s,
-            %(RETROVISOR_FOTOCROMICO)s,
-            %(RETROVISOR_ELETRICO)s,
-            %(RODAS_LIGA_LEVE)s,
-            %(SENSOR_DE_CHUVA)s,
-            %(SENSOR_DE_ESTACIONAMENTO)s,
-            %(TETO_SOLAR)s,
-            %(TRACAO_QUATRO_POR_QUATRO)s,
-            %(TRAVAS_ELETRICAS)s,
-            %(VIDROS_ELETRICOS)s,
-            %(VOLANTE_REG_ALTURA)s,
-            %(COMBUSTIVEL)s,
-            %(DATA_CARGA)s
-        );
-        '''
