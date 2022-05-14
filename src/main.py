d7476258060cb13694dc6146a0a224425529463f
toskosz <thiago.tokarski2001@gmail.com>
2022-05-13 22:38:53 -0300

    Adicionei uma notificacao por email em caso de erro na pipeline

--- src/main.py
+++ src/main.py
@@ -2,24 +2,28 @@ from autoline.autoline_extract import AutolineExtract
 from autoline.autoline_transform import AutolineTransform
 from webmotors.webmotors_extract import WebmotorsExtract
 from webmotors.webmotors_transform import WebmotorsTransform
+from util.error_email import notify_erro
 import warnings
 
 def run() -> None:
-    autoline_extract = AutolineExtract()
-    print("Extração inicializada.")
-    data = autoline_extract.run(20)
-    print("Extração finalizada.")
+    try:
+        autoline_extract = AutolineExtract()
+        print("Extração inicializada.")
+        data = autoline_extract.run(20)
+        print("Extração finalizada.")
 
-    autoline_transform = AutolineTransform()
-    autoline_transform.run(data)
-    
-    webmotors_extract = WebmotorsExtract()
-    print("[LOG] Extração inicializada.")
-    data = webmotors_extract.run(20)
-    print("[LOG] Extração finalizada.")
-    
-    webmotors_transform = WebmotorsTransform()
-    webmotors_transform.run(data)
+        autoline_transform = AutolineTransform()
+        autoline_transform.run(data)
+
+        webmotors_extract = WebmotorsExtract()
+        print("[LOG] Extração inicializada.")
+        data = webmotors_extract.run(20)
+        print("[LOG] Extração finalizada.")
+        
+        webmotors_transform = WebmotorsTransform()
+        webmotors_transform.run(data)
+    except:
+        notify_erro()
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
